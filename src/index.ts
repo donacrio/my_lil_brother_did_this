5d5f83aca28de459b19dd3864b838ba3ebe9ffd1
Donatien Criaud <donatien.criaud@gmail.com>
2025-05-03 23:17:53 +0200

    feat: use flattenjs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import p5 from 'p5';
 import { sketch } from './sketch';
 
-// Create p5 instance with the sketch
 new p5(sketch);
--- src/sketch.ts
+++ src/sketch.ts
@@ -1,22 +1,10 @@
+// src/sketch.ts
 import p5 from 'p5';
 
 export const sketch = (p: p5) => {
-  // Canvas setup
-  p.setup = () => {
-    p.createCanvas(window.innerWidth, window.innerHeight);
-    p.background(20);
-  };
+  p.setup = () => {};
 
-  // Drawing loop
-  p.draw = () => {
-    p.fill(255, 50);
-    p.noStroke();
-    p.ellipse(p.mouseX, p.mouseY, 20, 20);
-  };
+  p.draw = () => {};
 
-  // Window resize handler
-  p.windowResized = () => {
-    p.resizeCanvas(window.innerWidth, window.innerHeight);
-    p.background(20);
-  };
+  p.windowResized = () => {};
 };
